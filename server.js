19761220f3775bebee6b72feec189d6d5a069621
antorachea <antorachea@gmail.com>
2017-06-13 15:07:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/htmlpage', function(req,res){
+    res.sendFile(path.join(__dirname, 'ui', 'htmlpage.html'));
+});
+
 app.get('/art-one', function(req,res){
     res.send('first article will be procieded by the server');
 });

860e6cb454c2248333219e3fb4d3b45ac9e785dd
antorachea <antorachea@gmail.com>
2017-06-14 10:13:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,9 +1,69 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+ 
 var app = express();
 app.use(morgan('combined'));
+var artOne={
+    title: "Anto - Article one Serverjs",
+    Heading: "Article One",
+    date: '14 jun 1983',
+    content:  `
+    <p>
+            Paragraph 1:
+            This is the content for my first article. This is the content for my first article. This is the content for my first article
+            </p>
+            
+            <p>
+            Paragraph 2:
+                This isthe second paragraph
+            </p>
+            <p>
+                Paragraph 3:
+                This is the third paragraph
+            </p>'
+    
+};
+
+function createTemplate (data){
+var title=data.title;
+var date=data.date;
+var Heading=data.heading;
+var content=data.content;
+
+var htmlTemplate='
+
+<html>
+    <head>
+        <title>${title}</title>    
+<meta name='viewport' content='width-device-width, initial-scale=1' >
+<link href="/ui/style.css" rel="stylesheet" />
+
+    </head>
+    <body>
+        <div class='stylecontainer'>
+        <div>
+            <a href="/">Home</a>
+        </div>
+        <hr/>
+        <h3>
+            ${Heading}
+        </h3>
+        <div>
+            ${date}
+        </div>
+        
+        <div>
+            ${content}
+            
+        </div>
+        </div>
+    </body>
+</html>
+
+';
+return htmlTemplate
+}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));

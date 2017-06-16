e7fc62e3251e81b0a68664ff88e960ceb72e5158
antorachea <antorachea@gmail.com>
2017-06-16 10:36:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,29 @@
 console.log('Loaded!');
 var button = document.getElementById('counter');
-var counter = 0;
+// var counter = 0;
 button.onclick = function(){
+  //create a request object
+  var request = new XMLHttpRequest();
   
+  //Capture the response and store it in a variable.
+  request.onreadystatechange = function(){
+ if (request.readystate === XMLHttpRequest.DONE){
+     if (request.status === 200){
+         var counter = request.responseText;
+         var span = document.getElementById('count');
+         span.innerHTML = counter.toString();
+         
+     }
+ }
+  };
+ 
+ //make the request
+ request.open('GET', 'http://antorachea.imad.hasura-app.io/counter', true);
+ request.send(null);
+ 
   //Render the varialbe in the current span
-  counter = counter + 1;
-  var span = document.getElementById('count');
-  span.innerHTML = counter.toString();
+//  counter = counter + 1;
+ // var span = document.getElementById('count');
+ 
+ // span.innerHTML = counter.toString();
 };

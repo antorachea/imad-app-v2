db70dd77864bad812aa86c6a778862ffd45d2fbb
antorachea <antorachea@gmail.com>
2017-06-16 09:52:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -126,6 +126,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter = 0;
+app.get('/counter', function(req,res){
+counter = counter + 1;
+res.send(counter.toString());
+});
 
 app.get('/:artNs', function (req, res){
    // articleName == article name

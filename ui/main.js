08c4711976d26eb26592761273dd09d3b73b7697
antorachea <antorachea@gmail.com>
2017-06-16 09:07:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ ele = document.getElementById('anto-text');
 ele.innerHTML = 'New Value';
 
 //move the image
-var img = document.getElementById('img');
+var img = document.getElementById('anto');
 img.onclick = function(){
   img.style.margingLeft = '100px';  
 };
